9f39fc562a776b318b48e64af46b84f90b54c709
katchet <katcheck@gmail.com>
2025-02-13 14:52:03 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,180 @@
+let bird;
+let gameOverFlag = false;
+let pipes =[];
+let pipeSpacing = 150;
+let nextPipe = 0;
+let pipeSpeed = 2.3;
+let speedIncreaseRate = 0.0001;
+let score = 0;
+let highScore = 0;
+let restartButton;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  bird = new Bird();
+  restartButton = createButton('PLAY');
+  restartButton.position(width / 2 - restartButton.width / 2, height / 2 + 50);
+  restartButton.mousePressed(restartGame);
+  restartButton.hide();
+  
+  gameOverFlag = true; // Start the game at the restart screen
+}
+
+function draw() {
+  background(248, 248, 248);
+  bird.update();
+  bird.display();
+
+  if (frameCount > nextPipe) {
+    pipes.push(new Pipe());
+    nextPipe = frameCount + pipeSpacing;
+  }
+
+  pipeSpeed += speedIncreaseRate;
+
+  for (let i = pipes.length - 1; i >= 0; i--) {
+    pipes[i].update();
+    pipes[i].display();
+
+    if (!gameOverFlag && pipes[i].x + pipes[i].width < bird.x &&!pipes[i].scored) {
+      score++;
+      pipes[i].scored = true;
+    }
+
+    if (bird.intersects(pipes[i])) {
+      gameOver();
+    }
+
+    if (pipes[i].x < -pipes[i].width) {
+      pipes.splice(i, 1);
+    }
+  }
+
+  if (bird.y - bird.size / 2 <= 0 || bird.y + bird.size / 2 >= height) {
+    gameOver();
+  }
+
+  textSize(24);
+  fill(255,255,255);
+  text("Score: " + score, width / 2, 50);
+  text("High Score: " + highScore, width / 2, 80);
+
+  if (gameOverFlag) {
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    fill(255, 255, 255);
+    text("FLAPAVISTA", width / 2, height / 2);
+    restartButton.show();
+  } else {
+    restartButton.hide();
+  }
+}
+
+function touchStarted() {
+  if (!gameOverFlag) {
+    bird.flap();
+  }
+}
+
+function restartGame() {
+  gameOverFlag = false;
+  bird = new Bird();
+  pipes =[];
+  nextPipe = 0;
+  pipeSpeed = 2.3;
+  score = 0;
+  restartButton.hide();
+}
+
+function gameOver() {
+  gameOverFlag = true;
+
+  if (score > highScore) {
+    highScore = score;
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+class Bird {
+  constructor() {
+    this.x = width / 4;
+    this.y = height / 2;
+    this.gravity = 0.4;
+    this.velocity = 0;
+    this.lift = -13;
+    this.size = 40;
+  }
+
+  update() {
+    this.velocity += this.gravity;
+    this.y += this.velocity;
+
+    if (this.y + this.size / 2 > height) {
+      this.y = height - this.size / 2;
+      this.velocity = 0;
+    }
+
+    if (this.y - this.size / 2 < 0) {
+      this.y = this.size / 2;
+      this.velocity = 0;
+    }
+  }
+
+  flap() {
+    this.velocity += this.lift;
+  }
+
+  display() {
+    noStroke();
+    fill(253, 253, 253);
+    square(this.x, this.y, this.size);
+  }
+
+  intersects(pipe) {
+    // Calculate the center of the bird
+    let birdCenterX = this.x + this.size / 2;
+    let birdCenterY = this.y + this.size / 2;
+
+    // Calculate the center of the pipe
+    let pipeCenterX = pipe.x + pipe.width / 2;
+    let pipeCenterY = pipe.y + pipe.height / 2;
+
+    // Calculate the distance between the centers
+    let distX = Math.abs(birdCenterX - pipeCenterX);
+    let distY = Math.abs(birdCenterY - pipeCenterY);
+
+    // Check if the distance is greater than the sum of the half-widths/heights
+    if (distX > (pipe.width / 2 + this.size / 2) || distY > (pipe.height / 2 + this.size / 2)) {
+      return false;
+    }
+
+    return true;
+  }
+}
+
+class Pipe {
+  constructor() {
+    this.x = width;
+    this.topBuffer = 110;
+    this.bottomBuffer = 110;
+    this.width = 50;
+    this.height = random(100, height - this.topBuffer - this.bottomBuffer - 100);
+    let pipeCenter = this.height / 2;
+    let availableSpace = height - this.topBuffer - this.bottomBuffer;
+    let centerY = random(this.topBuffer + pipeCenter, height - this.bottomBuffer - pipeCenter);
+    this.y = centerY - pipeCenter;
+  }
+
+  update() {
+    this.x -= pipeSpeed;
+  }
+
+  display() {
+    noStroke();
+    fill(251, 251, 251);
+    rect(this.x, this.y, this.width, this.height);
+  }
+}
