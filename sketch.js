96c36097e65819b9a3bd257040aa9dbc1dd821d0
katchet <katcheck@gmail.com>
2025-02-13 15:36:05 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -61,7 +61,7 @@ let bird;
         text("High Score: " + highScore, width / 2, height * 0.15);
 
         if (gameOverFlag) {
-          textSize(width * 0.08);
+          textSize(width * 0.11);
           textAlign(CENTER,CENTER);
           fill(255, 255, 255);
           text("FLAPAVISTA", width / 2, height / 2);
